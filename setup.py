cd5b32f2121245387bd8f89683f692bf22089aff
Thomas Maschler <tmaschler@wri.org>
2019-02-01 15:59:20 -0500

    setup

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ from setuptools import setup  # , Extension
 setup(
     name="raster2points",
     version="0.1.0",
-    description="Tool to convert rasters to CSV files",
+    description="Tool to convert rasters to points",
     package_dir={"": "src"},
-    author="thomas.maschler",
+    author="Thomas Maschler",
+    author_email="thomas.maschler@wri.org",
     license="MIT",
     install_requires=["rasterio", "pandas", "numba"],
     scripts=["src/raster2csv.py"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
