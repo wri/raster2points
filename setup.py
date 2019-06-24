c763618c52e2fb4d6a152fc52719e63439bc5085
Thomas Maschler <tmaschler@wri.org>
2019-06-24 18:34:19 +0200

    update dependencies in setup.py
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setup(
     name="raster2points",
-    version="0.1.4",
+    version="0.1.5",
     description="Tool to convert rasters to points",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -14,7 +14,14 @@ setup(
     author="Thomas Maschler",
     author_email="thomas.maschler@wri.org",
     license="MIT",
-    install_requires=["rasterio", "pandas", "numba", "parallelpipe"],
+    install_requires=[
+        "numba~=0.44.1",
+        "pandas ~=0.24.2",
+        "parallelpipe~=0.2.6",
+        "pre-commit~=1.17.0",
+        "pytest~=4.6.3",
+        "rasterio[s3]~=1.0.24",
+    ],
     scripts=["raster2points/raster2csv.py"],
     classifiers=[
         "Programming Language :: Python :: 3",
