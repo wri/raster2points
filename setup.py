c94ef8d929250c227b34fe8dd0a01c49bda76df1
Thomas Maschler <tmaschler@wri.org>
2020-01-06 13:21:47 -0500

    update version
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setup(
     name="raster2points",
-    version="0.1.7",
+    version="0.1.8",
     description="Tool to convert rasters to points",
     long_description=long_description,
     long_description_content_type="text/markdown",
